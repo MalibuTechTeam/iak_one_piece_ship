2e14386df89d974ac4d94fa3b63533fe8e87b64b
Manifest Bumper <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-17 16:13:02 +0000

    chore: bump manifest version to v1.0.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game         'gta5'
 
 name 'iak_one_piece_ship'
 author 'Malibù Tech'
-version      '1.0.0'
+version      '1.0.1'
 description 'Faithful reproduction of the famous Going Merry from the famous anime One Piece'
 
 this_is_a_map 'yes'
